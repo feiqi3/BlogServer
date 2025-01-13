5b1c4e5eac9ab8c5ae07e02ddd7cc346d79d459a
feiqi3 <zjgtzn@outlook.com>
2025-01-14 01:58:24 +0800

    feat: http server on going.

--- FeiLib/include/Http/FHTTPDef.h
+++ FeiLib/include/Http/FHTTPDef.h
@@ -1,5 +1,6 @@
 #ifndef FHTTPDEF_H
 #define FHTTPDEF_H
+#include "../FDef.h"
 
 namespace Fei {
 namespace Http {
@@ -23,7 +24,16 @@ enum class Version {
 };
 
 enum StatusCode{
-    
+//1xx
+    _100,
+//2xx
+    _200,_201,_202,_204,
+//3xx
+    _301,_302,_303,_304,_307,_308,
+//4xx
+    _400,_401,_403,_404,_405,_408,_429,
+//5xx    
+    _500,_501,_502,_505,
 };
 } // namespace Http
 } // namespace Fei
--- FeiLib/include/Http/FHttpRequestParser.h
+++ FeiLib/include/Http/FHttpRequestParser.h
@@ -1,9 +1,33 @@
 #ifndef FHTTPARSER_H
-#define FHTTPARSER_H   
+#define FHTTPARSER_H
+#include "FHttpDef.h"
+#include "../FBufferReader.h"
 
-class FHttpParser{
+#include <string>
+#include <map>
+
+namespace Fei {
+
+
+class F_API FHttpContext {
     public:
-    
+    private:
+    std::map<std::string,std::string> mHeaders;
 };
 
+class FHttpParser {
+public:
+  static FHttpContext parse(FBufferReader &inReader);
+
+private:
+  static FBufferView parseHost();
+  static FBufferView parseUserAgent();
+
+  //Method and method data.
+  static std::pair<Http::Method,FBufferView> parseMethod();
+  static Http::Version parseVersion();
+
+  static FBufferView parseBody();
+};
+} // namespace Fei
 #endif
