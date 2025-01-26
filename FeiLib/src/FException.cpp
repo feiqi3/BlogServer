612f541e6927e384ea1b87c68bf6fa53ef5fc76d
feiqi3 <zjgtzn@outlook.com>
2025-01-26 20:44:34 +0800

    feat: exception with stacktrace

---
+++ FeiLib/src/FException.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <sstream>
+
+#include "FDef.h"
+#include "FException.h"
+#include "absl/debugging/stacktrace.h"
+
+#define MAX_STACK_DEPTH 32
+
+Fei::FException::FException()
+{
+	void* stack[MAX_STACK_DEPTH];
+	int sizes[MAX_STACK_DEPTH];
+
+	uint32 realStackDepth = absl::GetStackFrames(stack, sizes, MAX_STACK_DEPTH, 1);
+	std::stringstream ss;
+	ss << "Calling stack:";
+	for (auto i = 0; i < realStackDepth; ++i) {
+		ss << "\n" << std::string((char*)stack[i], sizes[i]);
+	}
+	mStackTace = ss.str();
+}
+
+const char* Fei::FException::what() const
+{
+	return mStackTace.c_str();
+}
