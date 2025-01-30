f08e19344b927c91c703420c2953482f59600170
feiqi3 <zjgtzn@outlook.com>
2025-01-30 14:39:37 +0800

    feat

--- FeiLib/include/FException.h
+++ FeiLib/include/FException.h
@@ -5,15 +5,22 @@
 #include <exception>
 
 namespace Fei {
-
-	class F_API FException : public std::exception {
+	std::string F_API getStackTrace(uint32 skip);
+	
+	class FException : public std::exception {
 	public:
-		FException();
-
-		const char* what() const noexcept final override;
+		FException():mStackTace(getStackTrace(1)){
 		
-		const std::string& stackTrace()const { return mStackTace; }
-		virtual std::string reason()const { return""; }
+		}
+
+		inline const char* what() const noexcept final override
+		{
+			mErrorStr = reason() + std::string("\n") + mErrorStr;
+			return mErrorStr.c_str();
+		}
+		
+		inline const std::string& stackTrace()const { return mStackTace; }
+		inline virtual std::string reason()const { return""; }
 	private:
 		std::string mStackTace;
 		mutable std::string mErrorStr;
--- FeiLib/src/FException.cpp
+++ FeiLib/src/FException.cpp
@@ -7,22 +7,18 @@
 
 #define MAX_STACK_DEPTH 32
 
-Fei::FException::FException()
+std::string F_API Fei::getStackTrace(uint32 skip)
 {
 	void* stack[MAX_STACK_DEPTH];
 	int sizes[MAX_STACK_DEPTH];
 
-	uint32 realStackDepth = absl::GetStackFrames(stack, sizes, MAX_STACK_DEPTH, 1);
+	uint32 realStackDepth = absl::GetStackFrames(stack, sizes, MAX_STACK_DEPTH, 1 + skip);
+
 	std::stringstream ss;
 	ss << "Calling stack:";
 	for (auto i = 0; i < realStackDepth; ++i) {
 		ss << "\n" << std::string((char*)stack[i], sizes[i]);
 	}
-	mStackTace = ss.str();
-}
 
-const char* Fei::FException::what() const noexcept
-{
-	mErrorStr = reason() + std::string("\n") + mErrorStr;
-	return mErrorStr.c_str();
+	return ss.str();
 }
