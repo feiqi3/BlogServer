6723d541e413af3c78f92565ab7664e83e2c7493
qi fei <zjgtzn@outlook.com>
2025-02-06 11:44:06 +0800

    fix a little error

--- FeiLib/include/FSingleton.h
+++ FeiLib/include/FSingleton.h
@@ -8,10 +8,11 @@ namespace Fei {
 
 //Why use F_API here?
 // --> indicate the inline static member sInstance is come from another dll/exported out of this dll  
-// --> so there is only one single instance memory exists in multi-dlls   
-// --> BTW It's a Win32 problem only
+// --> or you might get multi instance.   
+// --> BTW It's a Win32 problem only -- because C++ Standard doesnt make rule about dylink, so its impl defined.
+// --> And for client thee shall not use this class as base for who will get a link error as punishment
 // --> Magic OwO 
-template <typename T> class FSingleton {
+template <typename T> class F_API FSingleton {
 public:
   FSingleton() {
     assert(sInstance == 0 && "Double new singleton.");
