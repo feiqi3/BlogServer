083c9f0cc04133402cfb4ae02f47c33e9490330f
qi fei <zjgtzn@outlook.com>
2025-02-07 17:26:25 +0800

    feat: route result cache pool.

--- FeiLib/src/Http/FRouter.cpp
+++ FeiLib/src/Http/FRouter.cpp
@@ -49,18 +49,91 @@ namespace Fei::Http {
 		}
 	};
 
-	using PathOrderQueue = tbb::concurrent_map<uint64,ControllerAndPatternPtr, __ControllerAndPatternCompare>;
+	struct __DoHandle {
+		__DoHandle(std::atomic_bool& in) :f(in) {
+			in = true;
+		}
+		~__DoHandle() {
+			f = false;
+		}
+		std::atomic_bool& f;
+	};
 
 	class __FRouterInner {
 	public:
+
 		__FRouterInner() {
 			mControllerOrderQueue.resize((uint32)Method::MAX_SIZE);
+			mRouteCaches.resize((uint32)Method::MAX_SIZE);
 			mControllerOrderQueueEraseLocks.reset(new std::mutex[(uint32)Method::MAX_SIZE]);
 		}
+		
+
 		std::mutex m_eraseLock;
+		std::mutex m_routeCacheEraseLock;
 		tbb::concurrent_map<std::string, FControllerPtr> mControllerMap;
+		
+		struct RouteCahce {
+			uint64 cacheTime;
+			FRouter::RouteResult result;
+		};
+		using RouteCacheMap = tbb::concurrent_map<std::string, RouteCahce>;
+		std::vector<RouteCacheMap> mRouteCaches;
+
+		using PathOrderQueue = tbb::concurrent_map<uint64, ControllerAndPatternPtr, __ControllerAndPatternCompare>;
 		std::vector<PathOrderQueue> mControllerOrderQueue;
 		std::unique_ptr<std::mutex[]> mControllerOrderQueueEraseLocks;
+
+		std::atomic_bool isHandleRoute = false;
+		std::atomic_bool isHandleUnreg = false;
+	
+	public:
+		void clearCache() {
+			FAUTO_LOCK(m_routeCacheEraseLock);
+			for (auto&& i : mRouteCaches) {
+				i.clear();
+			}
+		}
+		
+		void putCache(const std::string& str,Method method, const FRouter::RouteResult& in) {
+			uint64 timeNow = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+			mRouteCaches[(uint32)method].insert({str, RouteCahce{.cacheTime = timeNow, .result = in}});
+		}
+
+		void invalidCache(const std::string& str,Method method) {
+			auto& caches = mRouteCaches[(int)method];
+			auto itor = caches.find(str);
+			if (itor == caches.end()) {
+				return;
+			}
+			FAUTO_LOCK(m_routeCacheEraseLock);
+			caches.unsafe_erase(itor);
+		}
+
+		void checkCacheOverdue(uint64 msDue) {
+			uint64 timeNow = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+			for (int i = 0; i < (int)Method::MAX_SIZE; ++i) {
+				auto& caches = mRouteCaches[i];
+				for (auto itor = caches.begin(); itor != caches.end();) {
+					if (timeNow - itor->second.cacheTime > msDue) {
+						FAUTO_LOCK(m_routeCacheEraseLock);
+						itor = caches.unsafe_erase(itor);
+					}
+				}
+			}
+		}
+
+		bool getRouteInCache(const std::string& str, Method method, FRouter::RouteResult& out) {
+			auto& caches = mRouteCaches[(int)method];
+			auto itor = caches.find(str);
+			if (itor == caches.end()) {
+				return false;
+			}
+			uint64 timeNow = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+			itor->second.cacheTime = timeNow;
+			out = itor->second.result;
+			return true;
+		}
 	};
 
 	void FRouter::RegisterController(const std::string& str, FControllerPtr controller)
@@ -91,13 +164,24 @@ namespace Fei::Http {
 	FRouter::RouteResult FRouter::route(Method method, const std::string& path)
 	{
 		RouteResult res{};
+		//Spin wait.
+		while (_dp->isHandleUnreg);
+		//----------//
+
 		{
+			__DoHandle handle(_dp->isHandleRoute);
+			if (_dp->getRouteInCache(path, method, res)) {
+				Logger::instance()->log("FRouter", lvl::trace, "{} with method: {} Find in cache.", path,methodToStr(method));
+				return res;
+			}
+
 			auto& controllers = _dp->mControllerOrderQueue[(uint32)method];
 			for (auto&& [key,val] : controllers) {
 				if (val->PathMatcher->isMatch(path, res.pathVariable)) {
 					Logger::instance()->log("FRouter", lvl::trace, "{} match path pattern {}",path, val->PathMatcher->getOriginPattern());
 					res.controllerFunc = val->ControllerFunc;
 					res.controllerSave = val->ControllerBase;
+					_dp->putCache(path, method, res);
 					return res;
 				}
 			}
@@ -161,6 +245,12 @@ namespace Fei::Http {
 
 	void FRouter::unregController(const std::string& controllerName)
 	{
+		__DoHandle handle(_dp->isHandleUnreg);
+		//Spin wait.
+		while (_dp->isHandleRoute);
+
+		_dp->clearCache();
+
 		FControllerPtr controllerPtr = 0;
 		{
 			auto itor = _dp->mControllerMap.find(controllerName);
@@ -175,6 +265,7 @@ namespace Fei::Http {
 				_dp->mControllerMap.unsafe_erase(itor);
 			}
 		}
+
 		for (auto queueIdx = 0; queueIdx < _dp->mControllerOrderQueue.size(); ++queueIdx) {
 			auto& queue = _dp->mControllerOrderQueue[queueIdx];
 			auto& eraselock = _dp->mControllerOrderQueueEraseLocks[queueIdx];
@@ -192,4 +283,5 @@ namespace Fei::Http {
 
 		}
 	}
+
 }
