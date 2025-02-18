a5b991a598e8baa9511937ea86dcced82c16628f
qi fei <zjgtzn@outlook.com>
2025-02-18 20:27:08 +0800

    feat: server ssl support

--- Server/src/Server/server.cpp
+++ Server/src/Server/server.cpp
@@ -1,19 +1,21 @@
 #include "FeiLibIniter.h"
 #include "server.h"
+#include "Http/FHttpServer.h"
 #include "Service/Filter.h"
 #include <string>
 #include <thread>
 const std::string ResourceDir =  SERVER_RESOURCE_DIR;
 const std::string ConfigDir = ResourceDir + "config/serverConfig";
 const std::string WebDir = ResourceDir + "web/";
+const std::string SSLFileDir = ResourceDir + "SSL/";
 
 Blog::Server::Server()
 {
 	Fei::FeiLibInit();
-
+	Fei::Http::FHttpServer::initSSLenv(SSLFileDir + "cert.pem", SSLFileDir + "private.pem");
 	server = new Fei::Http::FHttpServer(10);
 	server->addListenPort(80);
-	server->addListenPort(443);
+	server->addSSLPort(443);
 }
 
 void Blog::Server::run()
