47b2b6d449545a294a673f33bcf458eb3eaf7337
feiqi3 <zjgtzn@outlook.com>
2025-04-08 02:09:37 +0800

    fix.

--- Server/src/Core/ServerBasicDef.h
+++ Server/src/Core/ServerBasicDef.h
@@ -6,7 +6,7 @@ namespace Blog{
     const std::string BlogResourcePath = SERVER_RESOURCE_DIR;
     const std::string BlogWebPath = SERVER_RESOURCE_DIR "web/";
     const std::string BlogWebAssetsPath = SERVER_RESOURCE_DIR "web/assets/";
-    const std::string BlogWebPagePath = SERVER_RESOURCE_DIR "web/page";
+    const std::string BlogWebPagePath = SERVER_RESOURCE_DIR "web/page/";
     const std::string BlogDataBasePath = SERVER_RESOURCE_DIR "database/blog.db";
 };
 
