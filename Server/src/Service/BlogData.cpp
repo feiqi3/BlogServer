a734913692bd292904cf3d7e37a410b3e856e8fb
feiqi3 <zjgtzn@outlook.com>
2025-04-24 02:01:47 +0800

    fix.

--- Server/src/Service/BlogData.cpp
+++ Server/src/Service/BlogData.cpp
@@ -2,19 +2,21 @@
 #include "DAO/DataBaseOperation.h"
 #include "DAO/ORM.h"
 #include <cstdint>
+#include <string>
 #include "DAO/ORM.h"
 #include "Model/Status.h"
 namespace Blog {
 BlogData::BlogData() {
   auto updateState = DatabaseOperation::instance()->Prepare(
       "INSERT INTO BlogStatus(name, data) "
-      "VALUES(?, ?) "
-      "ON CONFLICT(name) DO UPDATE "
-      "SET data = BlogStatus.data "
-      "RETURNING data;");
-      DatabaseOperation::instance()->ReExec(updateState, "view_times",0);
+      "VALUES(\"view_times\", \"0\") "
+      "ON CONFLICT(name) DO NOTHING"
+      "; ");
+      DatabaseOperation::instance()->ReExec(updateState);
       updateState->excute();
-      this->mBlogTotalViewTimes = updateState->getInteger64(0);
+      Query<std::string> q(FIELD(Model::BlogStatus, data));
+      q.From("BlogStatus").Where(FIELD(Model::BlogStatus, name) == "view_times");
+      this->mBlogTotalViewTimes = std::stoull(q.exec().getVector()[0]);
       Query<uint64_t> query;
       query.Select(SelectCount()).From("Posts");
       mBlogTotalBlogs = query.exec().getVector()[0];
@@ -38,7 +40,8 @@ BlogData::BlogData() {
             return query;
         };
         thread_local auto q = genQuery();
-        q.exec((uint64_t)mBlogTotalViewTimes,std::string("view_times"));
+        auto res = q.exec(std::to_string((uint64_t)mBlogTotalViewTimes),std::string("view_times")).getResult();
+        res->excute();
         thread_local auto q2 = genQueryForTotalBlogs();
         q2.exec();
         this->mBlogTotalBlogs = q2.getVector()[0];
